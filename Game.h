95c62a2e15dbb37dc7eb239a5c682986bf9b1a15
ChrisCardoz <ccardoz1@jhu.edu>
2016-09-27 19:32:48 -0400

    screen class

---
+++ Game.h
@@ -0,0 +1,23 @@
+/*
+* S.W.A.G. 
+* Chris, Devin, Kevin, William 
+* 600.255 Intro to Video Game Design
+*
+*/
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <string>
+#include "Movable.h"
+#include "AutoMovable.h"
+#include "Screen.h"
+
+#ifndef _GAME_H
+#define _GAME_H
+
+class Game: public Screen {}
+
+#endif
---
+++ Screen.cpp
@@ -0,0 +1,26 @@
+/*
+* S.W.A.G. 
+* Chris, Devin, Kevin, William 
+* 600.255 Intro to Video Game Design
+*
+*/
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <string>
+#include "Movable.h"
+#include "AutoMovable.h"
+
+
+Screen::Screen(){}
+
+Screen::~Screen(){}
+
+int Screen::input(int key, int dt){}
+
+void Screen::draw(SDL_Renderer);
+
+
